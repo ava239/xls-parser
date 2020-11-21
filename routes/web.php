4d9d11eca33f3488dfa9dabd75ea1a30e4349033
ava <ava239@yandex.ru>
2020-11-21 21:17:38 +0300

    Remove basic models unnecessary actions

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome')->name('welcome');
 
 Route::resource('files', FileController::class)->only(['index', 'create', 'store']);
-Route::resource('rows', RowController::class);
+Route::resource('rows', RowController::class)->only(['index']);
