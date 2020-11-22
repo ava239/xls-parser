395857587dca397033eb225dc50150c2d3e77af7
ava <ava239@yandex.ru>
2020-11-23 01:55:44 +0300

    Optimize memory usage for rows controller

--- app/Http/Controllers/RowController.php
+++ app/Http/Controllers/RowController.php
@@ -3,14 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\Row;
+use DB;
 use Illuminate\Http\Request;
 
 class RowController extends Controller
 {
     public function index()
     {
-        $dates = Row::distinct('import_date')->get('import_date')->paginate(10);
-        $rows = Row::whereIn('import_date', $dates->values())
+        $dates = DB::table('rows')
+            ->distinct('import_date')
+            ->get('import_date')
+            ->map(fn($date) => $date->import_date)
+            ->paginate(10);
+
+        $rows = Row::whereIn('import_date', $dates)
             ->get(['import_id', 'import_name', 'import_date'])
             ->groupBy('import_date')
             ->map(function ($val, $key) {
